066559195133a4c72349bdf32a4ba4223ae41e76
FrenkensteinsCode <145868868+FrenkensteinsCode@users.noreply.github.com>
2023-10-02 18:08:24 +0200

    Fix removal of old backups

--- W10Backup.ps1
+++ W10Backup.ps1
@@ -11,6 +11,7 @@ $body_on_success = "SUCCESS_MESSAGE"
 $TimeStamp = get-date -f yyyyMMddhhmm
 $Source = "SOURCE_DIR" # e.g. "C:\Users\John\Documents\*"
 $Destination = "DEST_DIR" + $TimeStamp
+$BackupBaseDir = "DEST_DIR"
 $DateToDelete = (Get-Date).AddDays(-2) # Set delete date to two days
 
 # Backup process
@@ -19,8 +20,8 @@ Copy-Item -Path $Source -Destination $Destination -Force -Recurse
 if(-not $?) {
     Send-MailMessage -Credential $cred -UseSsl -From $sender -To $recipient -Subject 'Backup incomplete' -Body $body_on_failure
 } else {
+    Get-ChildItem -Path $BackupBaseDir -Directory | Where-Object { $_.Name -match "^\d{12}$" -and $_.Name -lt (Get-Date $DateToDelete -Format "yyyyMMddHHmm")} | Remove-Item -Recurse -Force # Remove backups of two days prior recursively
     Send-MailMessage -Credential $cred -UseSsl -From $sender -To $recipient -Subject 'Backup completed' -Body $body_on_success
 }
 
-# Remove backups of two days prior recursively
-Get-ChildItem $Destination -Recurse | Where-Object { $_.LastWriteTime -lt $DateToDelete } | Remove-Item -Recurse -Force
+
